ecdb10f5dd7d8c82483705291d1db7012084f805
Imadi <Imadi@bk.ru>
2020-11-20 19:20:03 +0400

    новые задания НБ

---
+++ авто объявление.py
@@ -0,0 +1,7 @@
+n = int(input())
+text = []
+for i in range[input]:
+    text.append(input())
+count = int(input())
+for i in range(count):
+    print(text[int(input()) - 1])
---
+++ автоматическое обявление.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = 0
+frasi = []
+while a != n:
+    c = input()
+    frasi.append(c)
+n = int(input())
+o = 0
+while o < n:
+    k = int(input())
+    print(frasi[k])
