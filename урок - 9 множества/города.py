34a672e2f8ab2a44ff3227b11eeed076b5e34dfe
Imadi_PC <Imadi@bk.ru>
2020-11-29 18:47:26 +0400

    Языки – 0.py закончен

--- урок - 9 множества/города.py
+++ урок - 9 множества/города.py
@@ -1,8 +1,9 @@
-s1 = input().lower()
-while True:
-    s2 = input().lower()
-    if s1[-1] != s2[0]:
-        print(s2)
-        break
-    s1 = s2
-
+kol = int(input())
+goroda = set()
+for i in range(kol):
+    goroda.add(input())
+gorod = input()
+if gorod in goroda:
+    print("TRY ANOTHER")
+else:
+    print("OK")
