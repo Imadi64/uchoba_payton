fbb78ad7e1e8c4b3d435774b098cbcef678e9a35
Imadi <Imadi@bk.ru>
2020-12-14 23:29:00 +0400

    вот

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-molochi = [1, 2, 3]
-for z in molochi:
-    print(z)
+a = int(input())
+b = int(input())
+
+while a != 0 or b != 0:
+    n = int(input()) - 1
+    c = int(input())
+
+    if n:
+        b -= c
+        print(a, b)
+    else:
+        a -= c
+        print(a, b)
+
+# 10
+# 10
+# 1
+# 5
+# 2
+# 10
+# 1
+# 5
