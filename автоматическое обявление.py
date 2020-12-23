d70c1be4635ea4547bee15be5c32304adfb18a24
Imadi <Imadi@bk.ru>
2020-12-23 11:21:36 +0400

    сделано

--- автоматическое обявление.py
+++ автоматическое обявление.py
@@ -1,11 +1,27 @@
-n = int(input())
-a = 0
-frasi = []
-while a != n:
-    c = input()
-    frasi.append(c)
-n = int(input())
-o = 0
-while o < n:
-    k = int(input())
-    print(frasi[k])
+x = True
+while x:
+    num = int(input())
+    deistvie = input()
+    if deistvie == '!':
+        if num >= 0:
+            numm = 1
+            for i in range(1, num + 1):
+                numm *= i
+            print(numm)
+    elif deistvie != 'x':
+        numm = int(input())
+        if deistvie == '+':
+            print(num + numm)
+        elif deistvie == '-':
+            print(num - numm)
+        elif deistvie == '*':
+            print(num * numm)
+        elif deistvie == '/':
+            if numm > 0:
+                print(num // numm)
+        elif deistvie == '%':
+            if numm > 0:
+                print(num % numm)
+    else:
+        print(num)
+        x = False
