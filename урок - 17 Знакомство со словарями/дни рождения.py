167c738a49755e901401bea8e64d21a816b8b544
Imadi_PC <Imadi@bk.ru>
2021-01-03 17:19:16 +0400

    *

--- урок - 17 Знакомство со словарями/дни рождения.py
+++ урок - 17 Знакомство со словарями/дни рождения.py
@@ -1,15 +1,20 @@
 chislo_odn = int(input())
-napomin_d_r = dict()
+napomin_d_r = {"янв": [], "фев": [], "мар": [], "апр": [], "май": [],
+               "июн": [], "июл": [], "авг": [], "сен": [], "окт": [],
+               "ноя": [], "дек": []}
 for i in range(chislo_odn):
     odnokl = input().split(" ")
-    mes = odnokl[0]
-    napomin_d_r[mes] = odnokl[-1]
+    mes = odnokl[2]
+    napomin_d_r[mes].insert(0, odnokl[0])
+
 vop_odn = int(input())
 vop = []
 for j in range(vop_odn):
     vop.append(input())
+
 for i in vop:
-    for key, value in napomin_d_r.items():
-        if value == i:
-            print(key, end=" ")
-    print()
+    if i in napomin_d_r:
+        if napomin_d_r[i] == []:
+            print()
+        else:
+            print(" ".join(napomin_d_r[i]))
