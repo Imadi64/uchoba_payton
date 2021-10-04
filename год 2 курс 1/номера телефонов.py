7a422d8ba337efcfcbaa784224dea8f8b5888747
Imadi-PS <Imadi@bk.ru>
2021-10-04 18:48:53 +0400

    задача доделана

--- год 2 курс 1/номера телефонов.py
+++ год 2 курс 1/номера телефонов.py
@@ -1,21 +1,54 @@
 nomer_nekorrekt = input()
 nomer = []
 nomer_korrekten = []
+sk1 = 0
+sk2 = 0
+sifri = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+a = 1
 
 for i in nomer_nekorrekt:
     nomer.append(i)
 
-# if (nomer_nekorrekt[0] == '8'):
-#     print('+7' + nomer_nekorrekt[1:-1])
-#
-# elif (nomer_nekorrekt[0] == '+' and nomer_nekorrekt[1] == '7'):
-#     print('+7' + nomer_nekorrekt[2:-1])
+if nomer_nekorrekt[0] != '8':
+    if nomer_nekorrekt[0] == '+' and nomer_nekorrekt[1] == '7':
+        nomer_nekorrekt = 'error'
 
-print(nomer)
+for i in nomer:
+    if i == '(':
+        sk1 += 1
+    if i == ')':
+        sk2 += 1
+
+if sk1 + sk2 != 2:
+    if sk1 + sk2 != 0:
+        nomer_nekorrekt = 'error'
+
 for i in range(len(nomer)):
-    if (nomer[i] == '(') or (nomer[i] == ')') or \
-            (nomer[i] == ' ') or (nomer[i] == '-'):
-        del nomer[i]
+    if nomer[i] != nomer[-1]:
+        if nomer[i] == '-' and nomer[i + 1] == '-':
+            nomer_nekorrekt = 'error'
+
+for i in range(len(nomer)):
+    if nomer_nekorrekt == 'error':
+        break
+    else:
+        if nomer[i] in sifri:
+            nomer_korrekten.append(nomer[i])
+
+for i in nomer_korrekten:
+    if nomer_nekorrekt == 'error':
+        print('error')
+        break
+    else:
+
+        if (len(nomer) == 11) and (a != 0):
+            print('+7', end='')
+            a = 0
+        else:
+            print(i, end='')
+
+
+
 
 
 # не удалть а добавлять числа в новый список
