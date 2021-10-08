dc76ed88d10f2403942e377a915335a5c516e0ec
Imadi-PS <Imadi@bk.ru>
2021-10-08 18:49:50 +0400

    ннн

--- классные занятия.py
+++ классные занятия.py
@@ -1,26 +1,9 @@
-class TooLargeError(Exception):
-    pass
+import csv
 
-
-def boats(*a):
-    h = 0
-     = []
-    for i in a:
-        if i % 21 != 0:
-            nysh_lodki.append(i)
-        if not (isinstance(i, int)):
-            raise TypeError('Incorrect argument')
-        elif  >= 1000000:
-            raise TooLargeError('All values cannot be so large')
-        elif len(nysh_lodki) == 0:
-            raise ZeroDivisionError('No matching numbers')
-
-
-    for i in nysh_lodki:
-        h += i
-        t = h / len(nysh_lodki)
-    return round(t, 2)
-
-
-data = [168, 42, 198, 63, 152, 3, 14]
-print(boats(*data))
+with open('vps.csv', encoding='utf8') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+    procent = int(input())
+    for index, row in enumerate(reader):
+        if index > 0:
+            if int(row[4]) >= procent:
+                print(row[0])
